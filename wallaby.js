544b286e0e221d4846c2baba044ed3dd35b77276
Artem Govorov <artem.govorov@gmail.com>
2017-01-09 12:42:50 +1000

    Update wallaby.js

--- wallaby.js
+++ wallaby.js
@@ -17,9 +17,15 @@ module.exports = function (wallaby) {
         compilers: {
             "**/*.ts?(x)": wallaby.compilers.typeScript({ module: 'commonjs' })
         },
+        preprocessors: {
+            '**/*.snap': file => (
+                file.rename(file.path.replace('.tsx', '.js').replace('.ts', '.js')),
+                file.content)
+        },
         testFramework: "jest",
         setup: function () {
             const jestConfig = require("./package.json").jest;
+            delete jestConfig.moduleFileExtensions;
             wallaby.testFramework.configure(jestConfig);
         }
     };
